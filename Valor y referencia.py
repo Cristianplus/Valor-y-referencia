d19b03ee208b1a6bb9503a6b6add3e07aff04eee
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-04 02:03:00 -0500

    actualización 04Jul2025

--- Valor y referencia.py
+++ Valor y referencia.py
@@ -44,3 +44,32 @@ def my_list_fun(my_list: list):
 my_list_c = [10,20]
 my_list_fun(my_list_c)
 print(my_list_c)
+
+# Por valor
+
+def value(value_a: int, value_b: int) -> tuple:
+    temp = value_a
+    value_a = value_b
+    value_b = temp
+    return value_a, value_b
+
+my_int_d = 10
+my_int_e = 20
+my_int_f, my_int_g = value(my_int_d, my_int_e)
+
+print(f"{my_int_d}, {my_int_e}")
+print(f"{my_int_f}, {my_int_g}")
+
+# Por referencia
+
+def ref(ref_a: list, ref_b: list) -> tuple:
+    temp = ref_a
+    ref_a = ref_b
+    ref_b = temp
+    return ref_a, ref_b
+
+my_list_e = [10, 20]
+my_list_f = [30, 40]
+my_list_g, my_list_h = ref(my_list_e, my_list_f)
+print(f"{my_list_e}, {my_list_f}")
+print(f"{my_list_g}, {my_list_h}")
